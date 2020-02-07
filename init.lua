71b08411c2fb21d1f75909b7630d03322295b950
Victor Gridnevsky <vic.6r1d@gmail.com>
2020-02-07 15:19:18 +0300

    Adds more glass recipes for a soft and hard glowing glass

--- init.lua
+++ init.lua
@@ -1,3 +1,14 @@
+-- 4 soft-glowing glasses using a basic craft recipe and a mese block
+minetest.register_craft({
+	output = 'new_glass:super_steel_framed_obsidian_glass 4',
+	recipe = {
+		{'default:obsidian_glass', 'default:obsidian_glass', 'default:steel_ingot'},
+		{'default:obsidian_glass', 'default:obsidian_glass', 'default:steel_ingot'},
+		{'default:steel_ingot', 'default:steel_ingot', 'default:mese'},
+	}
+})
+
+-- One soft-glowing glass from a steel framed one and a meselamp
 minetest.register_craft({
 	output = 'new_glass:super_steel_framed_obsidian_glass',
 	recipe = {
@@ -5,6 +16,7 @@ minetest.register_craft({
 	}
 })
 
+-- One hard-glowing glass from a steel framed one and two mese lamps
 minetest.register_craft({
 	output = 'new_glass:ultra_steel_framed_obsidian_glass',
 	recipe = {
@@ -12,6 +24,15 @@ minetest.register_craft({
 	}
 })
 
+-- One hard-glowing glass from a soft-glowing one and a mese lamp
+minetest.register_craft({
+	output = 'new_glass:ultra_steel_framed_obsidian_glass',
+	recipe = {
+		{'new_glass:super_steel_framed_obsidian_glass', 'default:meselamp'}
+	}
+})
+
+-- Node definition for a soft glowing glass
 minetest.register_node("new_glass:super_steel_framed_obsidian_glass", {
 	description = "Super Steel Framed Obsidian Glass",
 	drawtype = "glasslike_framed",
@@ -25,6 +46,7 @@ minetest.register_node("new_glass:super_steel_framed_obsidian_glass", {
         light_source = 11,
 })
 
+-- Node definition for a hard glowing glass
 minetest.register_node("new_glass:ultra_steel_framed_obsidian_glass", {
 	description = "Ultra Steel Framed Obsidian Glass",
 	drawtype = "glasslike_framed",
