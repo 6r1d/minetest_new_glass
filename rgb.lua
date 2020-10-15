bfbccbba21a4d64690fd221d1c8519694ba9d768
Victor Gridnevsky <vic.6r1d@gmail.com>
2020-10-15 14:17:54 +0300

    Adds reset functionality

--- rgb.lua
+++ rgb.lua
@@ -17,18 +17,25 @@ local digiline_action = function(pos, node, channel, msg)
   local setchannel = minetest.get_meta(pos):get_string("channel")
   -- Ignore other digiline channels
   if channel ~= setchannel or type(msg) ~= "table" then return end
-  -- Enable / disable glow
-  if (msg.switch ~= nil) and (type(msg.switch) == "string" or type(msg.switch) == "number") then
-    if msg.switch == 'on' or msg.switch == 1 then
-      minetest.swap_node(pos, {name = "new_glass:rgb_on" })
-    else
-      minetest.swap_node(pos, {name = "new_glass:rgb_off" })
-    end
-  end
   -- Set color
   if (msg.color ~= nil) and (type(msg.color) == "string" or type(msg.color) == "number") then
     node.param2 = msg.color
     minetest.swap_node(pos, node)
+  end
+  -- Enable / disable glow
+  if (msg.switch ~= nil) and (type(msg.switch) == "string" or type(msg.switch) == "number") then
+    if msg.switch == 'on' or msg.switch >= 1 then
+      minetest.swap_node(pos, {name = "new_glass:rgb_on", param2 = node.param2 })
+    else
+      minetest.swap_node(pos, {name = "new_glass:rgb_off", param2 = node.param2 })
+    end
+  end
+  -- Reset handler (int)
+  if (msg.reset ~= nil) and (type(msg.reset) == "number") then
+    if msg.reset >= 1 then
+      -- I am hardcoding a param2, because for some reason it has a default value
+      minetest.swap_node(pos, {name = "new_glass:rgb_off", param2 = 240 })
+    end
   end
 end
 
@@ -40,6 +47,24 @@ end
 local handle_receive_fields = function(pos, formname, fields, sender)
     local meta = minetest.get_meta(pos)
     if fields.channel then meta:set_string("channel", fields.channel) end
+end
+
+-- Override for unifieddyes implementation of on_dig,
+-- because I was not able to use it.
+local on_dig_rgb = function(pos, node, digger)
+    -- No player - no drop, may be incompatible with digtrons
+    if not digger then return end
+    -- Retrieve metadata
+    local playername = digger:get_player_name()
+    local inv = digger:get_inventory()
+    local item = ItemStack("new_glass:rgb_off")
+    -- Give player an item or drop it
+    if inv:room_for_item("main", item) then
+      inv:add_item("main", item)
+    else
+      minetest.item_drop(item, digger, pos)
+    end
+    minetest.remove_node(pos)
 end
 
 local register_rgb_glass = function(node_name, light_value)
@@ -58,10 +83,12 @@ local register_rgb_glass = function(node_name, light_value)
     airbrush_replacement_node = "new_glass:ultra_steel_framed_obsidian_glass_tinted",
     use_texture_alpha = true,
     groups = {
-      cracky = 3, oddly_breakable_by_hand = 3, ud_param2_colorable = 1
+      cracky = 3,
+      oddly_breakable_by_hand = 3,
+      ud_param2_colorable = 1
     },
     sounds = default.node_sound_glass_defaults(),
-    on_dig = unifieddyes.on_dig,
+    on_dig = on_dig_rgb,
     light_source = light_value,
     -- Set formspec for digiline channel selection
     on_construct = handle_construct,
@@ -91,8 +118,4 @@ end
 if minetest.get_modpath("unifieddyes") and minetest.get_modpath("digilines") then
   register_rgb_glass("new_glass:rgb_off", 0)
   register_rgb_glass("new_glass:rgb_on",  14)
-  -- Forcibly register RGB craftitem
-  --minetest.register_craftitem("new_glass:rgb_off", {
-  --  description = "RGB glass",
-  --})
 end
